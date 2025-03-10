7b27d67f379a1e06278eca4335ba47886ab22073
eugeniosaintemarie <eugenio.saintemarie@gmail.com>
2025-03-10 20:17:49 -0300

    footer fix-header

--- components/header.tsx
+++ components/header.tsx
@@ -1,9 +1,6 @@
 export default function Header() {
   return (
     <header className="bg-primary text-primary-foreground p-4">
-      <div className="container mx-auto">
-        <h1 className="text-xl font-bold">Generala Tracker</h1>
-      </div>
     </header>
   )
 }
