557dbba14bb3ad74578d0fa8a5a7e73ac849a8e6
eugeniosaintemarie <eugenio.saintemarie@gmail.com>
2025-03-10 20:33:34 -0300

    footer url

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,7 +4,11 @@ export default function Footer() {
   return (
     <footer className="bg-background text-muted-foreground p-4 mt-auto">
       <div className="container mx-auto">
-        <p className="text-center">∃ugenio © {currentYear}</p>
+        <p className="text-center">
+          <a href="/" className="hover:text-foreground transition-colors">
+            ∃ugenio © {currentYear}
+          </a>
+        </p>
       </div>
     </footer>
   )
