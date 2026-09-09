04ca069a14dd1f962e555918352c6bc6b9589f82
Eugenio Sainte Marie <26188808+eugeniosaintemarie@users.noreply.github.com>
2026-09-08 21:50:36 -0300

    footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -22,7 +22,7 @@ export default function Footer() {
         dangerouslySetInnerHTML={{
           __html: [
             '(() => {',
-            '  const siteBaseURL = "https://eugeniosm.com?ref=generalatracker".replace(/\\/$/, "");',
+            '  const siteBaseURL = "https://portfolio.eugeniosm.com/?ref=ref=generalatracker".replace(/\\/$/, "");',
             '  const buildRepoURL = (repoName, query = "") => {',
             '    const url = new URL(repoName + "/", siteBaseURL + "/");',
             '    if (query) {',
